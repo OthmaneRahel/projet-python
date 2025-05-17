31b04e7fa0aad6618025411fc8fd90fe8051763c
Hiba-saghir <Hiba.saghir@emsi-edu.ma>
2025-05-17 19:28:01 +0100

    hiba tasks

---
+++ hiba tasks/urls.py
@@ -0,0 +1,27 @@
+from django.urls import path
+from . import views 
+
+
+
+urlpatterns=[
+    path('',views.home,name='home'),
+    path('inscription/', views.inscription ,name='inscription'),
+
+    path('login/', views.LoginPage.as_view(), name='login_page'),           # GET: formulaire
+    path('home/', views.AuthUtilisateur.as_view(), name='home_user'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
+
+    path('annonces/ajouter/', views.ajouter_annonce, name='ajouter_annonce'),
+    path('annonces/', views.afficher_annonce_categorie, name='liste_annonce'),
+    path('mes-annonces/', views.mes_annonces, name='mes_annonces'),
+    path('modifier_annonce/<int:annonce_id>/', views.modifier_annonce, name='modifier_annonce'),
+
+    path('annonce/<int:annonce_id>/', views.detail_annonce.as_view(), name='detail_annonce'),
+
+    path('Profil_User/<int:user_id>', views.Profil_User, name='Profil_User'),
+    path('Profil_Modifier/', views.Profil_modifier, name='Profil_Modifier'),
+
+    path('user/messages/<int:user_id>',views.EnvoyerMessageAuUser.as_view(), name='messageToUser'),
+    path('message/<user_id>',views.messageUserAuUser, name='chat_actif'),
+
+]
---
+++ hiba tasks/views.py
@@ -0,0 +1,201 @@
+
+import os
+from django.http import HttpResponse
+from django.conf import settings
+from django.shortcuts import render, redirect
+from django.views import View
+from .forms import InscriptionForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import get_object_or_404, render, redirect
+from django.views import View
+from .models import User, Message,Categorie
+from .forms import BaseProfileForm, InscriptionForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+# Create your views here.
+
+
+# def home(request):
+#  return render(request, 'base.html')
+
+def inscription(request):
+    
+    if request.method == 'POST':
+        form = InscriptionForm(request.POST, request.FILES)
+        
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Inscription réussie! Vous pouvez maintenant vous connecter.')
+            return redirect('login_page')
+        else:
+         print(form.errors) 
+    else:
+        
+        form = InscriptionForm()
+        
+    return render(request, 'Utilisateur/inscription.html', {'form': form})
+
+class LoginPage(View):
+   def get(self, request):
+    # if 'adm' in request.path:
+    #     return render(request, 'Administrateur/loginPage.html')
+    return render(request, 'Utilisateur/login.html')
+
+class AuthUtilisateur(View):
+    def post(self, request):
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request, username=email, password=password)
+        
+        if user is not None:
+            login(request, user)
+            categories = Categorie.objects.all()
+            annonces = Annonce.objects.all().order_by('-datePublication')[:6]  # Récupère les 6 dernières annonces
+            return render(request, 'Utilisateur/homePage.html',{'annonces': annonces, 'MEDIA_URL': settings.MEDIA_URL,"categories":categories})  # Redirection après succès
+        else:
+            messages.error(request, 'Email ou mot de passe invalide.')
+            return redirect('login_page')  # On retourne à la page de connexion
+
+    def get(self, request):
+        if request.user.is_authenticated:
+            categories = Categorie.objects.all()
+            annonces = Annonce.objects.all().order_by('-datePublication')[:6]  # Récupère les 6 dernières annonces
+            return render(request, 'Utilisateur/homePage.html',{'annonces': annonces, 'MEDIA_URL': settings.MEDIA_URL,"categories":categories})  # Redirection après succès
+        return redirect('login_page')
+
+class LogoutView(View):
+    def get(self, request):
+        if request.user.is_authenticated:
+            logout(request)
+            return redirect('home')
+            # return render(request, 'base.html', {'message': 'Vous avez été déconnecté avec succès.'})
+        return render(request, 'Utilisateur/homePage.html')
+        
+def Profil_User(request, user_id):
+    return render(request, 'Utilisateur/Profil_User.html', {
+        'user': get_object_or_404(User, id=user_id),
+        'user_ads': Annonce.objects.filter(user_id = request.user.id),
+        'user_ads_count': Annonce.objects.filter(user_id = request.user.id).count(),
+        'categories' : Categorie.objects.all(),
+        # 'user_favorites_count': fa.objects.filter(user_id = request.user.id).count(),
+        'MEDIA_URL':settings.MEDIA_URL
+    })
+
+def Profil_modifier(request ):
+    if request.method == 'POST':
+        form = BaseProfileForm(request.POST, request.FILES, instance=request.user)
+        
+        if form.is_valid():
+            form.save()
+            return redirect('Profil_User',user_id=request.user.id)
+        else:
+            print(form.errors)
+    else:
+        form = BaseProfileForm(instance=request.user)
+    return render(request, 'Utilisateur/Modifier_Profil.html', {'form': form})   
+
+
+from django.http import JsonResponse
+from .models import Annonce
+import json
+from django.core.files.storage import default_storage
+
+def ajouter_annonce(request):
+    if request.method == 'POST':
+            titre = request.POST.get('titre')
+            description = request.POST.get('description')
+            categorie = request.POST.get('categorie_id')
+            prix = float(request.POST.get('prix'))
+            is_premieum = request.POST.get('isPremieum') == 'on'
+            noms_images = []
+            images = request.FILES.getlist('images')
+            for image in images:
+                path = os.path.join('annonces', image.name)
+                saved_path = default_storage.save(path, image)
+                noms_images.append(saved_path)
+            annonce = Annonce.objects.create(
+                titre=titre,
+                description=description,
+                prix=prix,
+                user_id=request.user.id,
+                categorie_id=categorie,
+                isPremieum=is_premieum,
+                statut='active',
+                images=noms_images
+            )
+            return JsonResponse({'success': True, 'annonce_id': annonce.id})
+
+from .forms import ModifierAnnonceForm
+
+def modifier_annonce(request, annonce_id):
+    annonce = get_object_or_404(Annonce, id=annonce_id)
+    
+    if request.method == 'POST':
+        form = ModifierAnnonceForm(request.POST, instance=annonce)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "L'annonce a été mise à jour avec succès!")
+            return redirect('mes_annonces')
+        else:
+            print("Erreurs du formulaire:", form.errors)
+
+    else:
+        form = ModifierAnnonceForm(instance=annonce)
+    
+    return render(request, 'Utilisateur/Modifier_Anc.html', {'form': form,'annonce': annonce})
+
+def afficher_annonce_categorie(request,categorie_id):
+
+    annonces = Annonce.objects.filter(categorie_id=categorie_id)
+    return render(request, 'Utilisateur/test.html', {
+        'annonces': annonces,
+        'MEDIA_URL': settings.MEDIA_URL
+})
+
+def mes_annonces(request):
+    annonces = Annonce.objects.filter(user=request.user).order_by('-datePublication')
+    categories = Categorie.objects.all()
+    return render(request, 'Utilisateur/Mes_annonces.html', {'annonces': annonces,'MEDIA_URL': settings.MEDIA_URL,'categories': categories})
+
+from .models import Annonce
+
+def home(request):
+    categories = Categorie.objects.all()
+    annonces = Annonce.objects.all().order_by('-datePublication')[:6]  # Récupère les 6 dernières annonces
+    return render(request, 'base.html', {'annonces': annonces, 'MEDIA_URL': settings.MEDIA_URL,"categories":categories})
+
+from django.shortcuts import get_object_or_404
+
+class detail_annonce(View):
+    def get(self, request, annonce_id):
+        annonce = get_object_or_404(Annonce, id=annonce_id)
+        return render(request, 'Utilisateur/detail_annonce.html',
+                       {'annonce': annonce,
+                        'user_ads_count': Annonce.objects.filter(user_id = request.user.id).count(),
+                        'MEDIA_URL': settings.MEDIA_URL})
+
+class EnvoyerMessageAuUser(View):
+    def get(self,request,user_id):
+        userMessages = Message.objects.filter(userIdSource=request.user.id,userIdDesti=user_id)
+        user_des = User.objects.get(id = user_id)
+        return render(request,"Utilisateur/interfaceChat.html",{"user_actif":user_des,"userMessages":userMessages})
+    
+    def post(self,request,user_id):
+        contenu = request.POST.get("contenu")
+        user_des = request.POST.get("user_id")
+        print(user_des)
+        Message.objects.create(
+            userIdDesti_id = user_des,
+            contenu=contenu,
+            userIdSource_id = request.user.id,
+        )
+        return redirect(f"/user/messages/{user_id}")
+    
+def messageUserAuUser(request,user_id):
+    print(user_id)
+    # return HttpResponse(user_id)
+    listeUser = Message.objects.filter(userIdSource = request.user.id)
+    # # user = User.objects.get(id=userid)
+    userMessages = Message.objects.filter(userIdSource=request.user.id,userIdDesti=user_id)
+    return render(request,"Utilisateur/interfaceChat.html",{"listeUser":listeUser})
